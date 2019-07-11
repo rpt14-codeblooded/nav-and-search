2368646e602466706e3d2b483ce3d78c632b2f53
Manuel Duran <manuelduran@Manuels-MacBook-Air.local>
2019-07-11 09:47:46 -0500

    style(app and TopBar): Added styling to make it resemble the eBay header

--- client/app.jsx
+++ client/app.jsx
@@ -1,5 +1,29 @@
 import React from 'react';
 import TopBar from './components/TopBar.jsx';
+import style from 'styled-components';
+
+const StyledHeader = style.header`
+  min-width: 320px;
+  position: relative;
+  margin: 0;
+  padding: 35px 0 10px 0;
+  white-space: nowrap;
+  line-height: 1;
+  font-size: 14px;
+  color: #333;
+  text-align: left;
+  display: block;
+  border: 0;
+  font-weight: normal;
+`
+
+const StyledContainer = style.div`
+  background: url(https://ir.ebaystatic.com/pictures/aw/pics/cmp/ds3/imgbg.jpg) repeat 0 0 #f8f8f8;
+  color: #333;
+  font-size: 14px;
+  font-family: "Helvetica neue",Helvetica,Arial,Sans-serif;
+  line-height: 1;
+`
 
 class App extends React.Component {
   constructor(props) {
@@ -9,7 +33,11 @@ class App extends React.Component {
 
   render() {
     return (
-      <TopBar />
+      <StyledContainer>
+        <StyledHeader>
+          <TopBar />
+        </StyledHeader>
+      </StyledContainer>
     )
   }
 }
--- client/components/TopBar.jsx
+++ client/components/TopBar.jsx
@@ -1,10 +1,78 @@
 import React from 'react';
+import style from 'styled-components';
 
+const StyledContainer = style.div`
+    background-color: #fff;
+    border-bottom: 1px solid #ccc;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 30px;
+    box-sizing: initial;
+    display: block;
+`
+
+const StyledLeftUl = style.ul`
+  margin-top: 1px;
+  text-align: left;
+  margin: 0;
+  padding: 0;
+  list-style: none outside none;
+  display: block;
+  margin-block-start: 1em;
+  margin-block-end: 1em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  padding-inline-start: 40px;
+`
+
+const StyledFirstLi = style.li`
+list-style: none;
+background: 0;
+padding: 0;
+display: inline-block;
+font-size: 12px;
+margin: 0;
+border: 0;
+font-weight: normal;
+text-align: -webkit-match-parent;
+`
+const StyledDividerLi = style.li`
+list-style: none;
+display: inline-block;
+border-left: 1px #909090 solid;
+font-size: 12px;
+padding: 0 9px 0 11px;
+margin: 0;
+border: 0;
+font-weight: normal;
+font-size: 100%;
+`
+
+const StyledFinalLi = style.li`
+display: none;
+vertical-align: top;
+padding-left: 15px !important;
+height: 30px;
+overflow: hidden;
+padding-top: 7px;
+margin-top: -7px;
+background: 0;
+box-sizing: initial;
+list-style: none;
+font-size: 12px;
+padding: 0 9px 0 11px;
+margin: 0;
+border: 0;
+font-weight: normal;
+text-align: -webkit-match-parent;
+`
 function TopBar(props) {
   return (
-    <div id="container">
-      <ul id="leftUl">
-        <li>
+    <StyledContainer>
+      <StyledLeftUl>
+        <StyledFirstLi>
           <span>
             Hi!
             <a href="#"> Sign in </a>
@@ -13,20 +81,20 @@ function TopBar(props) {
               <a href="#"> register</a>
             </span>
           </span>
-        </li>
-        <li>
+          </StyledFirstLi>
+        <StyledDividerLi>
           <a href="#"> Daily Deals</a>
-        </li>
-        <li>
+        </StyledDividerLi>
+        <StyledDividerLi>
           <a href="#"> Gift Cards</a>
-        </li>
-        <li>
+        </StyledDividerLi>
+        <StyledDividerLi>
           <a href="#"> Help and Contact</a>
-        </li>
-        <li>
+        </StyledDividerLi>
+        <StyledFinalLi>
           <img src="https://ir.ebaystatic.com/cr/v/c1/59591_070819_Prime_Generic_Doodle_150x30_FINAL_V2.gif"></img>
-        </li>
-      </ul>
+        </StyledFinalLi>
+      </StyledLeftUl>
       <ul>
         <li>
           <a href="#"> Sell</a>
@@ -35,13 +103,13 @@ function TopBar(props) {
           <a href="#"> My eBay</a>
         </li>
         <li>
-          <div> Bell Picture here</div>
+          <button></button>
         </li>
         <li>
-          <div> Shopping Cart Picture here</div>
+          <button></button>
         </li>
       </ul>
-    </div>
+    </StyledContainer>
   )
 }
 
